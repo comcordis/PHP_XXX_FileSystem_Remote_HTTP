5ba6f9f13f6f68c167afe19a606ed845f2e97da0
vinceMeens <v.w.a.meens@comcordis.com>
2013-04-11 11:57:11 +0200

    Various changes

--- XXX_HTTP_Request.php
+++ XXX_HTTP_Request.php
@@ -1,5 +1,47 @@
 <?php
 
+function curl_redir_exec($ch)
+    {
+        static $curl_loops = 0;
+        static $curl_max_loops = 20;
+        if ($curl_loops++ >= $curl_max_loops)
+        {
+            $curl_loops = 0;
+            return FALSE;
+        }
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        list($header, $data) = explode("\n\n", $data, 2);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($http_code == 301 || $http_code == 302)
+        {
+            $matches = array();
+            preg_match('/Location:(.*?)\n/', $header, $matches);
+            $url = @parse_url(trim(array_pop($matches)));
+            if (!$url)
+            {
+                //couldn't process the url to redirect to
+                $curl_loops = 0;
+                return $data;
+            }
+            $last_url = parse_url(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+            if (!$url['scheme'])
+                $url['scheme'] = $last_url['scheme'];
+            if (!$url['host'])
+                $url['host'] = $last_url['host'];
+            if (!$url['path'])
+                $url['path'] = $last_url['path'];
+            $new_url = $url['scheme'] . '://' . $url['host'] . $url['path'] . ($url['query']?'?'.$url['query']:'');
+            curl_setopt($ch, CURLOPT_URL, $new_url);
+            debug('Redirecting to', $new_url);
+            return curl_redir_exec($ch);
+        } else {
+            $curl_loops=0;
+            return $data;
+        }
+    }
+
 function curl_exec_follow($ch, &$maxredirect = null) {
   
   // we emulate a browser here since some websites detect
@@ -166,7 +208,7 @@ abstract class XXX_HTTP_Request
 				curl_setopt($curlHandler, CURLOPT_CAINFO, $ssl);
 			}		
 			
-			$result = curl_exec_follow($curlHandler);
+			$result = curl_redir_exec($curlHandler);
 			
 			curl_close($curlHandler);
 		}
